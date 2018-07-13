c48589c310212ba5ec2c5c80f70fec9b5d5e6ee6
DXkite <DXkite@163.com>
2018-07-13 22:33:21 +0800

    添加：蛇移动

---
+++ 08-snake-draw-and-move/views.py
@@ -0,0 +1,126 @@
+from pygameui import Button
+import pygame
+from snake import Snake
+from pygame import KEYDOWN
+
+
+class View:
+    screen = None
+
+    """
+    界面初始化
+    """
+
+    def __init__(self, screen):
+        self.screen = screen
+
+    """
+    界面绘制处理
+    """
+
+    def on_draw(self):
+        pass
+
+    """
+    界面事件分发
+    """
+
+    def on_event(self, event):
+        pass
+
+
+class GameView(View):
+    block = None
+    food = None
+    snake = None
+
+    def __init__(self, context):
+        View.__init__(self, context.screen)
+        # 加载基础资源
+        self.block = pygame.image.load('img/block.jpg')
+        self.food = pygame.image.load('img/food.png')
+        self.w = 12
+        self.h = 12
+        self.BLOCK_SIZE = self.block.get_size()
+        context.screen = pygame.display.set_mode(
+            (self.w*self.BLOCK_SIZE[0], self.h * self.BLOCK_SIZE[1]), 0, 32)
+        # 初始化蛇
+        self.snake = Snake(0, 0)
+
+    """""""""""""""""""""
+    在某个位置绘制一个块
+    """""""""""""""""""""
+
+    def draw_surface(self, pos, surface):
+        self.screen.blit(
+            surface, (pos[0]*self.BLOCK_SIZE[0], pos[1]*self.BLOCK_SIZE[1]))
+
+    def on_draw(self):
+        self.snake.on_draw(self)
+
+    def on_event(self, event):
+        if event.type == KEYDOWN:
+            self.snake.on_event(event)
+
+
+class GameOverView(View):
+    pass
+
+
+class MainView(View):
+
+    logo = None
+    btn_start = None
+    btn_exit = None
+    context = None
+
+    def __init__(self, context):
+        self.btn_start = Button('Start Game', self._on_start)
+        self.btn_exit = Button('Exit Game', self._on_exit)
+        self.context = context
+        self.logo = pygame.image.load('img/logo.png')
+        w, h = self.logo.get_size()
+        # 调整界面适应 Logo 大小以及显示按钮
+        self.context.screen = pygame.display.set_mode(
+            (int(w * 1.5), int(h * 2)), 0, 32)
+        # 设置界面标题
+        pygame.display.set_caption('Python Snake Game v1.0')
+        View.__init__(self, context.screen)
+
+    def on_draw(self):
+        # 绘制界面
+        self._on_draw()
+        # 绘制按钮
+        self.btn_start.on_draw(self.context.screen)
+        self.btn_exit.on_draw(self.context.screen)
+
+    def on_event(self, event):
+        # 监控各种事件
+        self.btn_start.on_event(event)
+        self.btn_exit.on_event(event)
+
+    def _on_exit(self):
+        # 控制退出状态
+        print('exit game')
+        self.context.running = False
+
+    def _on_start(self):
+        # 切换界面到游戏界面
+        print('start game')
+        self.context.view = GameView(self.context)
+
+    def _on_draw(self):
+        '''
+        计算各个按钮的位置以及Logo的位置
+        '''
+        s_w, s_h = self.context.screen.get_size()
+        l_w, l_h = self.logo.get_size()
+        x, y, bs_w, bs_h = self.btn_start.get_box()
+        x, y, be_w, be_h = self.btn_exit.get_box()
+        y_start = l_h + 50
+        '''
+        界面元素排版
+        '''
+        self.btn_start.pos = (s_w/2 - bs_w / 2, y_start)
+        self.btn_exit.pos = (s_w/2 - be_w / 2, y_start + bs_h + 4)
+        self.context.screen.blit(self.logo, (s_w/2-l_w/2, s_h/2-l_h/2))
